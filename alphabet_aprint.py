04fe52f29cdfe30ee7fd133d6b558fd164f0fda1
AnshulKumbhare <anshulkumbhare16@gmail.com>
2024-04-03 12:45:55 +0530

    Printing Alphabet A

---
+++ alphabet_aprint.py
@@ -0,0 +1,20 @@
+"""
+Write a Python program to print the alphabet pattern 'A'.
+Expected Output:
+
+  ***
+ *   *
+ *   *
+ *****
+ *   *
+ *   *
+ *   *
+"""
+
+for i in range(7):
+    if i == 0:
+        print(" *** ")
+    elif i == 3:
+        print("*****")
+    else:
+        print("*   *")

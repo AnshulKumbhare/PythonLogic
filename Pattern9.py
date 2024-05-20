05d9c5ee610bb0cc84f62db7eb8207d581dea5bb
AnshulKumbhare <anshulkumbhare16@gmail.com>
2024-05-20 15:42:21 +0530

    Pattern 9

---
+++ Pattern9.py
@@ -0,0 +1,8 @@
+for i in range(4):
+    for j in range(i + 1, 4):
+        print(" ", end=" ")
+
+    for k in range(i + 1):
+        print("*", end=" ")
+
+    print()
---
+++ PracticeMay/prblm_factorial.py
@@ -0,0 +1,7 @@
+n = int(input("Enter n: "))
+
+f = 1
+for i in range(1, n):
+    f = f * i
+
+print(f)
